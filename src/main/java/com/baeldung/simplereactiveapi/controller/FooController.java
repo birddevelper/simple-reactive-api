ec7b4458230070fbd2665605d6cee81bfbc1e734
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-14 09:16:53 -0700

    Add Foo Controller

---
+++ src/main/java/com/baeldung/simplereactiveapi/controller/FooController.java
@@ -0,0 +1,30 @@
+package com.baeldung.simplereactiveapi.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.simplereactiveapi.model.Foo;
+import com.baeldung.simplereactiveapi.service.FooService;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class FooController {
+	
+
+	@Autowired
+	private FooService fooService;
+	
+	// This endpoint emits a new Foo as JSON object every second
+    @GetMapping(value = "/foo")
+    public Flux<Foo> getFoo() {
+    	
+        return fooService.getFoo();
+    }
+    
+
+
+}
